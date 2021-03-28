8ef01fd45aa768f0ee7d032d7aca5b1358965ff6
myoungc <81394198+myoungc@users.noreply.github.com>
2021-03-28 18:10:30 +1100

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,25 +1,23 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: 'cfb3209403c4439ea36dd9cb3b3cf679',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Michael Young Jun Choi',
+  domain: 'www.michaelychoi.com',
+  author: 'Michael Choi',
 
   // open graph metadata (optional)
-  description: 'Example site description',
-  socialImageTitle: 'Transitive Bullshit',
-  socialImageSubtitle: 'Hello World! 👋',
+  description: 'It was all a dream',
+  socialImageTitle: 'MYC',
+  socialImageSubtitle: 'Lets gooooo 💪🏻',
 
-  // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  // social usernames (optional)twitter: 'transitive_bs',  github: 'transitive-bullshit',  linkedin: 'fisch2',
+  instagram: mick_choi
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
